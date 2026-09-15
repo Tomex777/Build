a052d694c30de89d675b6857a5e7b26b677feb91
Tomex777 <patrickcoker737@gmail.com>
2026-09-15 10:52:27 +0100

    Include manager bootstrap probe

--- nightmods/nightcore/settings.gradle.kts
+++ nightmods/nightcore/settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "NightCore"
-include(":app", ":xposed-stubs", ":target-probe", ":attacker-probe")
+include(":app", ":xposed-stubs", ":target-probe", ":attacker-probe", ":manager-probe")
