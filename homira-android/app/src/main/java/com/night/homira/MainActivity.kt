2b605a63d02cb867fac2677b1a0bbba26f520c61
Tomex777 <patrickcoker737@gmail.com>
2026-09-17 22:37:26 +0100

    Homira: launch voicemail and swipe-call UI

--- homira-android/app/src/main/java/com/night/homira/MainActivity.kt
+++ homira-android/app/src/main/java/com/night/homira/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.night.homira.ui.HomiraApp
+import com.night.homira.ui.HomiraProductionApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HomiraApp()
+            HomiraProductionApp()
         }
     }
 }
