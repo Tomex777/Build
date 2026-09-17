55bd06a080741c669068543ca1ad005ddc260834
Tomex777 <patrickcoker737@gmail.com>
2026-09-17 05:28:51 +0100

    Homira: add iOS app entry point

---
+++ homira-ios/Homira/HomiraApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct HomiraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
