9ee4da7ae68410ae1119f6ca1a11793df4983613
Tomex777 <patrickcoker737@gmail.com>
2026-09-18 06:20:25 +0100

    Homira: boot through live Supabase auth

--- homira-android/app/src/main/java/com/night/homira/MainActivity.kt
+++ homira-android/app/src/main/java/com/night/homira/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.night.homira.ui.HomiraProductionApp
+import com.night.homira.ui.HomiraAuthGate
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HomiraProductionApp()
+            HomiraAuthGate()
         }
     }
 }
