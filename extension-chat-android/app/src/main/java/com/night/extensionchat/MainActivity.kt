5b2ce86bad1731b5ffa967f2905777721040b3e8
Tomex777 <patrickcoker737@gmail.com>
2026-09-18 18:44:59 +0100

    Fix Extension Chat activity declaration

--- extension-chat-android/app/src/main/java/com/night/extensionchat/MainActivity.kt
+++ extension-chat-android/app/src/main/java/com/night/extensionchat/MainActivity.kt
@@ -7,7 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import com.night.extensionchat.ui.ExtensionChatApp
 import com.night.extensionchat.ui.ExtensionChatTheme
 
-// Visual validation build target.\nclass MainActivity : ComponentActivity() {
+// Visual validation build target.
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
