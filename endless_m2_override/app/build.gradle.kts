3ad42ef61dd01edaad0a5b8add6460970934843b
Tomex777 <patrickcoker737@gmail.com>
2026-09-18 13:49:27 +0100

    Endless M3: bump textured navigation build

--- endless_m2_override/app/build.gradle.kts
+++ endless_m2_override/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.night.endless"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0-m2"
+        versionCode = 3
+        versionName = "0.3.0-m3"
     }
 
     buildFeatures {
