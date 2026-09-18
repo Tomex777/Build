d086c5cf6de46d85e7c2411368b01093ffd5b44d
Tomex777 <patrickcoker737@gmail.com>
2026-09-18 13:32:08 +0100

    Homira: lower minimum SDK to 26

--- homira-android/app/build.gradle.kts
+++ homira-android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         applicationId = "com.night.homira"
-        minSdk = 36
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "0.1.0-call-ui"
