4b7a4f655a49245e652669831833b7943c1cfc57
Tomex777 <patrickcoker737@gmail.com>
2026-09-18 13:31:58 +0100

    Endless M2: add CelestialBody.kt

---
+++ endless_m2_override/app/src/main/java/com/night/endless/engine/scene/CelestialBody.kt
@@ -0,0 +1,19 @@
+package com.night.endless.engine.scene
+
+import com.night.endless.engine.math.Vec3d
+
+data class CelestialBody(
+    val id: String,
+    val name: String,
+    val radius: Double,
+    val orbitRadius: Double,
+    val orbitPeriodDays: Double,
+    val rotationHours: Double,
+    val color: FloatArray,
+    val parentId: String? = null,
+    val phaseRad: Double = 0.0,
+    val radiusKm: Double = 0.0,
+    val semiMajorAxisAu: Double? = null,
+    val description: String = "",
+    var position: Vec3d = Vec3d.ZERO
+)
