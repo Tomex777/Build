1545a8ede052fd2995adfb02aa5cdf0dfe53f7c8
Tomex777 <patrickcoker737@gmail.com>
2026-09-18 19:02:35 +0100

    Trigger Extension Chat visual render

--- extension-chat-android/app/src/main/java/com/night/extensionchat/MainActivity.kt
+++ extension-chat-android/app/src/main/java/com/night/extensionchat/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import com.night.extensionchat.ui.ExtensionChatApp
 import com.night.extensionchat.ui.ExtensionChatTheme
 
-// Visual validation build target.
+// Visual validation build target. CI visual render trigger.
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
