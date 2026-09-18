821184449d94098304a75113bfa6def5e228d31a
Tomex777 <patrickcoker737@gmail.com>
2026-09-18 15:11:22 +0100

    Endless M3.2: bump interaction tuning build

--- endless_m2_override/app/build.gradle.kts
+++ endless_m2_override/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.night.endless"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.1-m3"
+        versionCode = 5
+        versionName = "0.3.2-m3"
     }
 
     buildFeatures {
