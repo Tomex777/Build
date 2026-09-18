c466d3322f7792b3b06a12c4f3aea69ccef5bab3
Tomex777 <patrickcoker737@gmail.com>
2026-09-18 18:29:14 +0100

    Trigger Extension Chat visual validation build

--- extension-chat-android/app/src/main/java/com/night/extensionchat/MainActivity.kt
+++ extension-chat-android/app/src/main/java/com/night/extensionchat/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import com.night.extensionchat.ui.ExtensionChatApp
 import com.night.extensionchat.ui.ExtensionChatTheme
 
-class MainActivity : ComponentActivity() {
+// Visual validation build target.\nclass MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
