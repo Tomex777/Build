f7407bc6c260f26653834781c2c394c3a63c7e66
Tomex777 <patrickcoker737@gmail.com>
2026-09-19 20:48:20 +0100

    Align Night with Kotlin 2.2.10

--- whatsapp-ai-android/app/build.gradle.kts
+++ whatsapp-ai-android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id ("com.google.dagger.hilt.android")
     id ("kotlin-kapt")
     id("com.google.gms.google-services")
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10"
 
 }
 
