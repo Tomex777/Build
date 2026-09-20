867d8a355c8bf81927706ef6ddfdcb04bafe0fe6
Tomex777 <patrickcoker737@gmail.com>
2026-09-20 08:40:22 +0100

    diag: capture physical-device startup crashes

--- homira-android/app/src/main/java/com/night/homira/HomiraApplication.kt
+++ homira-android/app/src/main/java/com/night/homira/HomiraApplication.kt
@@ -2,4 +2,9 @@ package com.night.homira
 
 import android.app.Application
 
-class HomiraApplication : Application()
+class HomiraApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        HomiraCrashReporter.install(this)
+    }
+}
