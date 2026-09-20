b265b80e69acf96aea0d84b61b34ecc097059c0b
Tomex777 <patrickcoker737@gmail.com>
2026-09-20 11:44:52 +0100

    Endless M4: bump Mars approach build

--- endless_m2_override/app/build.gradle.kts
+++ endless_m2_override/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.night.endless"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.3.3-m3"
+        versionCode = 7
+        versionName = "0.4.0-m4"
     }
 
     buildFeatures {
