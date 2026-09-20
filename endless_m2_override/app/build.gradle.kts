378da738b54bece10dbb2fd3cd31f19e507db6e1
Tomex777 <patrickcoker737@gmail.com>
2026-09-20 11:23:26 +0100

    Endless M3.3: bump polish build

--- endless_m2_override/app/build.gradle.kts
+++ endless_m2_override/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.night.endless"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.3.2-m3"
+        versionCode = 6
+        versionName = "0.3.3-m3"
     }
 
     buildFeatures {
