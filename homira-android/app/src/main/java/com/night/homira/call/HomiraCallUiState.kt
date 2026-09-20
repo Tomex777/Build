01661518489f014a9e2b82a5219f971b73d99e59
Tomex777 <patrickcoker737@gmail.com>
2026-09-20 20:07:18 +0100

    video: expose picture-in-picture state to call UI

--- homira-android/app/src/main/java/com/night/homira/call/HomiraCallUiState.kt
+++ homira-android/app/src/main/java/com/night/homira/call/HomiraCallUiState.kt
@@ -1,6 +1,12 @@
 package com.night.homira.call
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
 object HomiraCallUiState {
     @Volatile
     var videoCallActive: Boolean = false
+
+    var pictureInPictureActive by mutableStateOf(false)
 }
