3414da7ffdecf14986acb87c87967c7c213f95a0
Tomex777 <patrickcoker737@gmail.com>
2026-09-25 16:25:54 +0100

    Use AGP built-in Kotlin plugin

--- annie-android/app/build.gradle.kts
+++ annie-android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -45,6 +44,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation("io.github.dokar3:quickjs-kt:1.0.15")
+    implementation("io.github.rosemoe:editor:0.24.6")
+    implementation("io.github.rosemoe:language-monarch:0.24.6")
+    implementation("io.github.dingyi222666.monarch:monarch-language-pack:1.0.2")
     implementation("io.coil-kt:coil-compose:2.7.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
     testImplementation("junit:junit:4.13.2")
