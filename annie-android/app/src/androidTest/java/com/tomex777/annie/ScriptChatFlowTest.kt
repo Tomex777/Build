ea1d5d5f30da49862082dfd488576b04c35043e0
Tomex777 <patrickcoker737@gmail.com>
2026-09-25 21:36:35 +0100

    Dismiss keyboard before asserting script editor visibility

--- annie-android/app/src/androidTest/java/com/tomex777/annie/ScriptChatFlowTest.kt
+++ annie-android/app/src/androidTest/java/com/tomex777/annie/ScriptChatFlowTest.kt
@@ -24,6 +24,8 @@ class ScriptChatFlowTest {
         compose.setContent { AnnieTheme { AnnieChat() } }
         compose.onNodeWithTag("composer_input").performTextInput("/scripts")
         compose.onNodeWithTag("send_message").performClick()
+        compose.runOnIdle { compose.activity.currentFocus?.clearFocus() }
+        compose.waitForIdle()
         compose.waitUntil(5_000) {
             compose.onAllNodesWithTag("script_studio").fetchSemanticsNodes().isNotEmpty()
         }
