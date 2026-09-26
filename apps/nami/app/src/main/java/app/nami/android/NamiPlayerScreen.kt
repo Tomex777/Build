b2125290fcb400755119c62f4d174e31b16afb5d
Tomex777 <patrickcoker737@gmail.com>
2026-09-26 07:07:19 +0100

    fix(nami): import player text overflow

--- apps/nami/app/src/main/java/app/nami/android/NamiPlayerScreen.kt
+++ apps/nami/app/src/main/java/app/nami/android/NamiPlayerScreen.kt
@@ -63,6 +63,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.WindowCompat
