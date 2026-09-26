ddaccdc6f67e90e1773ea5cbf511ec3cc2106aac
Tomex777 <patrickcoker737@gmail.com>
2026-09-26 17:17:47 +0100

    Add native editor Espresso coverage

--- annie-android/app/build.gradle.kts
+++ annie-android/app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2025.01.00"))
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
